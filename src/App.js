cef9f565d812faedbabedaeae8679bd7974c0c23
Michael Lee <ckmichael8@gmail.com>
2017-07-25 23:41:25 +0800

    Add Google Anlyatics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import ReactGA from 'react-ga';
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Router>
-          <Route path="/" component={Nav}></Route>
+        <Router onUpdate={ () => (ReactGA.pageview(window.location.pathname)) }>
+          <Route
+            path="/"
+            component={ Nav }></Route>
         </Router>
       </MuiThemeProvider>
-    );
+      );
   }
 }
 
