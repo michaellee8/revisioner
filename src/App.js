cdc69ec0c802cfa2d429cd7745e6341814790939
Michael Lee <ckmichael8@gmail.com>
2017-07-26 01:23:04 +0800

    Provide google anlyatic

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Nav from './components/Nav'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ReactGA from 'react-ga';
+
 
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Router onUpdate={ () => (ReactGA.pageview(window.location.pathname)) }>
+        <Router onUpdate={ () => (window.ga('send', 'pageview', window.location.pathname)) }>
           <Route
             path="/"
             component={ Nav }></Route>
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import './base'
-import ReactGA from 'react-ga';
 
 injectTapEventPlugin();
-ReactGA.initialize('UA-103158209-1');
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
