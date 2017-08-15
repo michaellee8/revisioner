95347eecf786f12ab8147e37fc0f14908d27cc69
michaellee8 <ckmichael8@gmail.com>
2017-08-15 10:47:56 +0800

    Implement question creator

--- src/components/Create/create.js
+++ src/components/Create/create.js
@@ -1,56 +1,111 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import Menu from 'material-ui/Menu';
-import MenuItem from 'material-ui/MenuItem';
+const Component = require("Component");
+const Menu = require("Menu.react");
+const MenuItem = require("MenuItem.react");
+const React = require("React");
+
+const axios = require("axios");
+const firebase = require("firebase");
+
+import React, { Component } from "react";
+import axios from "axios";
+import Menu from "material-ui/Menu";
+import MenuItem from "material-ui/MenuItem";
+import firebase from "firebase";
 
 class Create extends Component {
   state: {
-    questionSets: Array<any>
-  }
-  getQuestionSet(){
-    firebase.auth().currentUser.getToken(true).catch(err => {
-      console.log(err);
-      this.getQuestions();
-    }).then(authToken => {
-      return axios({
-        url: "/getQuestions/byUid",
-        method: "get",
-        baseURL: window.serverUrl,
-        params: {
-          authId: firebase.auth().currentUser.uid,
-          authToken: authToken,
-          fetchTimes: this.state.fetchTimes
-        }
+    questionSets: Array<any>,
+    selectedQuestionSetValue: string
+  };
+  getQuestionSet() {
+    firebase
+      .auth()
+      .currentUser.getToken(true)
+      .catch(err => {
+        console.log(err);
+        this.getQuestionSet();
       })
-    })
+      .then(authToken => {
+        return axios({
+          url: "/getQuestionSet",
+          method: "get",
+          baseURL: window.serverUrl,
+          params: {
+            authId: firebase.auth().currentUser.uid,
+            authToken: authToken
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        this.getQuestionSet();
+      })
+      .then(res => {
+        this.setState({ questionSets: res.data });
+      });
   }
   render() {
     return (
       <div>
         Select the question set
-        <Menu onChange={ (e, v) => {
-                           this.setState((prevState, props) => {
-                             var arr = JSON.parse(JSON.stringify(prevState.questionSets));
-                             var i;
-                             for (i = 0; i < arr.length; i++) {
-                               if (arr[i].value === v) {
-                                 arr[i].checked = true
-                               } else {
-                                 arr[i].checked = false;
-                               }
-                             }
-                             return {
-                               questionSets: arr
-                             };
-                           })
-                         } }>
-          { this.state.questionSets.map((v) => {
-              return (<MenuItem
-                        primaryText={ v.title }
-                        value={ v.id }
-                        checked={ v.checked ? true : false } />)
-            }) }
+        <Menu
+          onChange={(e, v) => {
+            this.setState((prevState, props) => {
+              var arr = JSON.parse(JSON.stringify(prevState.questionSets));
+              var i;
+              for (i = 0; i < arr.length; i++) {
+                if (arr[i].value === v) {
+                  arr[i].checked = true;
+                } else {
+                  arr[i].checked = false;
+                }
+              }
+              return {
+                questionSets: arr,
+                selectedQuestionSetValue: v
+              };
+            });
+          }}
+        >
+          {this.state.questionSets.map(v => {
+            return (
+              <MenuItem
+                primaryText={v.title}
+                value={v.id}
+                checked={v.checked ? true : false}
+              />
+            );
+          })}
         </Menu>
-      </div>);
+        <Editor
+          SetTitle={
+            this.state.questionSets.find(
+              element => element.value === this.state.selectedQuestionSetValue
+            ).title
+          }
+          SetTitle={
+            this.state.questionSets.find(
+              element => element.value === this.state.selectedQuestionSetValue
+            ).subtitle
+          }
+          SetId={
+            this.state.questionSets.find(
+              element => element.value === this.state.selectedQuestionSetValue
+            ).id
+          }
+          sumbitHandler={data => {
+            axios({
+              url: "/createQuestion",
+              method: "post",
+              baseURL: window.serverUrl,
+              data: data
+            }).catch(err => {
+              console.log(err);
+              alert("Cannot Sumbit Question");
+            });
+          }}
+        />
+      </div>
+    );
   }
 }
--- src/components/Create/editor.js
+++ src/components/Create/editor.js
@@ -1,14 +1,14 @@
 import React, { Components } from "react";
-import CorrectIcon from 'material-ui/svg-icons/action/done'
-import { Card, CardText, CardActions, CardTitle } from 'material-ui/Card'
-
+import CorrectIcon from "material-ui/svg-icons/action/done";
+import { Card, CardText, CardActions, CardTitle } from "material-ui/Card";
 
 class Editor extends React.Components {
   props: {
     SetTitle: string,
     SetSubtitle: string,
+    SetId: string
     sunbitHandler: Function
-  }
+  };
   state: {
     questionTitle: string,
     questionType: string,
@@ -16,7 +16,7 @@ class Editor extends React.Components {
     correctOption: number,
     option: Array<string>,
     currentOption: string
-  }
+  };
   constructor(props) {
     this.state.correctOption = 0;
     this.state.currentOption = "";
@@ -28,73 +28,87 @@ class Editor extends React.Components {
       questionType: this.state.questionType,
       questionText: this.state.questionText,
       correctOption: this.state.correctOption,
-      option: this.state.option
+      option: this.state.option,
+      setId: this.props.SetId
     });
   }
   render() {
-    return (<div>
-              <Card>
-                <CardHeader
-                  title={ SetTitle }
-                  SetSubtitle={ SetSubtitle } />
-                <CardText>
-                  <TextField
-                    hintText="Question Title"
-                    onChange={ (e, v) => this.setState({
-                                 questionTitle: v
-                               }) } />
-                  <br/>
-                  <TextField
-                    hintText="Question Type"
-                    onChange={ (e, v) => this.setState({
-                                 questionType: v
-                               }) } />
-                  <br/>
-                  <TextField
-                    hintText="Question Text"
-                    multiLine={ true }
-                    onChange={ (e, v) => this.setState({
-                                 questionText: v
-                               }) } />
-                  <br/>
-                  <div>
-                    First Option is correct
-                  </div>
-                  <List>
-                    { this.state.option.map((value, index) => {
-                        if (index === 0) {
-                          return (<div>
-                                    <CorrectIcon/>
-                                    { value }
-                                  </div>);
-                        } else {
-                          return (<div>
-                                    <WrongIcon/>
-                                    { value }
-                                  </div>);
-                        }
-                      }) }
-                  </List>
-                  <br/>
-                  <TextField
-                    hintText="New Option"
-                    value={ thi.state.currentOption }
-                    onChange={ (e, v) => this.setState({
-                                 currentOption: v
-                               }) } />
-                </CardText>
-                <CardAction>
-                  <FlatButton
-                    label="Add Option"
-                    onTouchTap={ (e) => this.setState((prevState, props) => ({
-                                   option: [...prevState.option, prevState.currentOption],
-                                   currentOption: ""
-                                 })) } />
-                  <FlatButton
-                    label="Sumbit"
-                    onTouchTap={ this.props.sumbitHandler(this.sumbit()) } />
-                </CardAction>
-              </Card>
-            </div>);
+    return (
+      <div>
+        <Card>
+          <CardHeader title={SetTitle} SetSubtitle={SetSubtitle} />
+          <CardText>
+            <TextField
+              hintText="Question Title"
+              onChange={(e, v) =>
+                this.setState({
+                  questionTitle: v
+                })}
+            />
+            <br />
+            <TextField
+              hintText="Question Type"
+              onChange={(e, v) =>
+                this.setState({
+                  questionType: v
+                })}
+            />
+            <br />
+            <TextField
+              hintText="Question Text"
+              multiLine={true}
+              onChange={(e, v) =>
+                this.setState({
+                  questionText: v
+                })}
+            />
+            <br />
+            <div>First Option is correct</div>
+            <List>
+              {this.state.option.map((value, index) => {
+                if (index === 0) {
+                  return (
+                    <div>
+                      <CorrectIcon />
+                      {value}
+                    </div>
+                  );
+                } else {
+                  return (
+                    <div>
+                      <WrongIcon />
+                      {value}
+                    </div>
+                  );
+                }
+              })}
+            </List>
+            <br />
+            <TextField
+              hintText="New Option"
+              value={thi.state.currentOption}
+              onChange={(e, v) =>
+                this.setState({
+                  currentOption: v
+                })}
+            />
+          </CardText>
+          <CardAction>
+            <FlatButton
+              label="Add Option"
+              onTouchTap={e =>
+                this.setState((prevState, props) => ({
+                  option: [...prevState.option, prevState.currentOption],
+                  currentOption: ""
+                }))}
+            />
+            <FlatButton
+              label="Sumbit"
+              onTouchTap={this.props.sumbitHandler(this.sumbit())}
+            />
+          </CardAction>
+        </Card>
+      </div>
+    );
   }
 }
