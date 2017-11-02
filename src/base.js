1d74895f8a343b79abc1304465bc913d33d9629d
michaellee8 <ckmichael8@gmail.com>
2017-11-02 12:18:21 +0800

    Update base.js

--- src/base.js
+++ src/base.js
@@ -1,12 +1,12 @@
 var Rebase = require('re-base');
 var firebase = require('firebase');
 var app = firebase.initializeApp({
-    apiKey: "AIzaSyCMtO5Lx8oDnIto0TguGttu6tslbtAlMDo",
-    authDomain: "revisioner-3c321.firebaseapp.com",
-    databaseURL: "https://revisioner-3c321.firebaseio.com",
-    projectId: "revisioner-3c321",
-    storageBucket: "revisioner-3c321.appspot.com",
-    messagingSenderId: "1035995091286"
+    apiKey: "AIzaSyAp9txxRcx-hL23qwCdNc3cfW9LuOnC02U",
+    authDomain: "michaellee8-sba-revisioner.firebaseapp.com",
+    databaseURL: "https://michaellee8-sba-revisioner.firebaseio.com",
+    projectId: "michaellee8-sba-revisioner",
+    storageBucket: "michaellee8-sba-revisioner.appspot.com",
+    messagingSenderId: "322651489781"
   });
 var base = Rebase.createClass(app.database());
 export default base;
